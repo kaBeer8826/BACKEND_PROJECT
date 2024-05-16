cd471b722ed9e07105087c76181a5448dde4f50b
kabeer <studykabeer@gmail.com>
2024-05-17 02:31:33 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asynHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=> next(err))
+    }
+}
+
+export {asynHandler}
+
+
+
+// same fucntionality
+// const asynHandler =(fn)=>async(req, res, next)=>{
+//     try {
+//         await fn (req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success :false,
+//             message : err.message
+//         })
+//     }
+// }
